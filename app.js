393b66c886af5385576c6abe6d4aa9c23e907795
Dean <deanhume@gmail.com>
2023-08-29 13:39:11 +0100

    Added logic to determine if entered age is appropriate

--- app.js
+++ app.js
@@ -59,6 +59,34 @@ function getCountryLocation(req) {
 }
 
 /**
+ * Determines if the age entered is safe for the title and returns the HTML
+ * @param {int} enteredAge 
+ * @param {string} location 
+ * @param {JSON} details 
+ * @returns 
+ */
+function determineAgeRating(enteredAge, location, details) {
+
+    let titleAgeRating = 0;
+    enteredAge = parseInt(enteredAge);
+
+    // First, determine the age rating for the title based on the location
+    if (location == "US") {
+        titleAgeRating = parseInt(details.rating.ESRB);
+    } else if (location == "GB") {
+        titleAgeRating = parseInt(details.rating.PEGI);
+    }
+
+    // Then decide if the title is safe for the entered age and return the HTML
+    if (enteredAge < titleAgeRating) {
+        return `<div class="relative bg-red-600 isolate px-6 lg:px-8"><div class="mx-auto max-w-2xl" style="padding-top: 8rem;padding-bottom: 5rem;"><div class="hidden sm:mb-8 sm:flex sm:justify-center"></div><div class="text-center"><h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-white">No, ${details.title} is not safe for a ${enteredAge} year old.</h1></div></div></div>`;
+    }
+    else if (enteredAge >= titleAgeRating) {
+        return `<div class="relative bg-green-600 isolate px-6 lg:px-8"><div class="mx-auto max-w-2xl" style="padding-top: 8rem;padding-bottom: 5rem;"><div class="hidden sm:mb-8 sm:flex sm:justify-center"></div><div class="text-center"><h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl text-white">Yes, ${details.title} is safe for a ${enteredAge} year old.</h1></div></div></div>`;
+    }
+}
+
+/**
  * Build the HTML details of the game title.
  * @param {string} id 
  * @param {string} location 
@@ -69,13 +97,25 @@ function buildTitleDetails(id, location, age) {
     const fileContent = buffer.toString();
 
     // Load the ratings details file
-    const detailsBuffer = fs.readFileSync(path.join(__dirname, `/title-details/${id}.html`));
+    const jsonDetails = fs.readFileSync(path.join(__dirname, `/title-details/${id}.json`));
+    let details = JSON.parse(jsonDetails);
+
+    const detailsBuffer = fs.readFileSync(path.join(__dirname, `/title-details/template.html`));
     let detailsContent = detailsBuffer.toString();
 
+    detailsContent = detailsContent.replace("<!--{{title}}-->", details.title);
+    detailsContent = detailsContent.replace("<!--{{studio}}-->", details.studio);
+    detailsContent = detailsContent.replace("<!--{{outline}}-->", details.outline);
+    detailsContent = detailsContent.replace("<!--{{contentIssues}}-->", details.contentIssues);
+    detailsContent = detailsContent.replace("<!--{{otherIssues}}-->", details.otherIssues);
     detailsContent = detailsContent.replace("<!--{{age}}-->", age);
+    detailsContent = detailsContent.replace("<!--{{imageUrl}}-->", `/images/${id}.png`);
 
     // Append the ratings details based on the location
     let htmlToReturn = fileContent.replace("<!--{{Details}}-->", detailsContent);
+
+    // Determine if the title is safe for the entered age
+    htmlToReturn = htmlToReturn.replace("<!--{{ageAppropriate}}-->", determineAgeRating(age, location, details));
 
     // Replace the age in the HTML file
     htmlToReturn = htmlToReturn.replace("<!--{{age}}-->", age);
