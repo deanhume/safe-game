70c164da9fb17c9b59f590e5cf1b9d610217d456
Dean <deanhume@gmail.com>
2023-09-13 11:09:51 +0100

    Added pills and improved drop down

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'));
+    res.sendFile(path.join(__dirname, '/index2.html'));
 })
 
 /**
